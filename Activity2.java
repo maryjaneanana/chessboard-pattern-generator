a4d75336caf9c88ba815b644d4b91efd5717f6b7
Mary Jane Añana <124349270+codewithjane@users.noreply.github.com>
2023-10-01 11:19:31 +0800

    Add files via upload

---
+++ Activity2.java
@@ -0,0 +1,38 @@
+package Activity2;
+
+import java.util.Scanner;
+
+public class Activity2 {
+public static void main(String[] args) {
+char[][] chessboard = new char[8][8];
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.println ("====== CHESSBOARD PATTERN GENERATOR ======");
+System.out.println("");
+System.out.println("");
+
+System.out.print("Enter the column coordinates: ");
+char column = scanner.next().charAt(0);
+System.out.print("Enter the row coordinates: ");
+int row = scanner.nextInt();
+
+switch (column) {
+case 'a': case 'c': case 'e': case 'g':
+if (row % 2 == 0)
+ System.out.println(column + "" + row + "  is white block");
+else
+ System.out.println(column + "" + row + "  is black block");
+break;
+case 'b': case 'd': case 'f': case 'h':
+if (row % 2 == 0)
+System.out.println(column + "" + row + "  is black block");
+else
+System.out.println(column + "" + row + "  is white block");
+break;
+default:
+System.out.println("Invalid column coordinates");
+break;
+}
+}
+}
